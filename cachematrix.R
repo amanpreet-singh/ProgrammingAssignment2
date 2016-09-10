4b98e25b7b4f1428c9a807f7fa20a7efd63160bc
Amanpreet Singh <amanpreet_singh@hotmail.com>
2016-09-11 00:24:17 +0530

    Assignment2_Aman: R Language: Cache Matrix Inverse
    This R script is submission for R language Assignment 2 (Week 3) by Amanpreet Singh.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## Below functions help to create a custom matrix which can cache its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix: Creates a special "matrix" object that can cache its inverse
+## Assumption: Input matrix should be a square invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setminv <- function(minv) m <<- minv
+        getminv <- function() m
+        list(set = set, get = get,
+             setminv = setminv,
+             getminv = getminv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getminv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setminv(m)
+        m
 }
